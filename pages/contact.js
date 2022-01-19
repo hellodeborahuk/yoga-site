d246e3a544cd364ea27650c0840093b92b37db3f
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-19 21:55:35 +0000

    Updated contact page with content.

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,15 @@
-
 const Contact = () => {
-    return ( 
-        <div><h1>Get in touch</h1>
+  return (
+    <div>
+      <h1>Get in touch</h1>
+      <p>If you have any questions please contact Debbie.</p>
+      <p>Email: debbiedannyoga@gmail.com </p>
+      <p>
+        Follow me on <a href="https://instagram.com/debbiedannyoga">Instagram</a> or{" "}
+        <a href="https://www.facebook.com/debbiedannyoga">Facebook</a>.
+      </p>
+    </div>
+  );
+};
 
-            </div>
-     );
-}
- 
 export default Contact;

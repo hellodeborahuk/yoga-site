47a8d2348b863f328285ae66c18071b873ff14bf
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-20 20:17:58 +0000

    Got blog data to display on [id] pages

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,31 +1,40 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, getDoc, doc } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCPvh9ILI1N7j3-Mt_eH-uvkX13cdwoDaI",
+  authDomain: "yoga-website-4eafe.firebaseapp.com",
+  projectId: "yoga-website-4eafe",
+  storageBucket: "yoga-website-4eafe.appspot.com",
+  messagingSenderId: "343308541111",
+  appId: "1:343308541111:web:2d5d9292baab0b264b4132",
+};
 
-export async function getStaticPaths() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
-  const paths = data.map(post => {
-        return {
-            params: { id: post.id.toString() }
-        }
-  })
-
+export const getStaticPaths = async () => {
+  const snapshot = await getDocs(collection(db, "posts"));
+  const paths = snapshot.docs.map((doc) => {
+    return {
+      params: { id: doc.id.toString() },
+    };
+  });
   return {
     paths,
-    fallback: false
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const docRef = doc(db, "posts", id);
+  const docSnap = await getDoc(docRef);
+
+  return {
+    props: { post: docSnap.data() }
   }
-}
-
-
-export async function getStaticProps(context) {
-    const id = context.params.id
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const data = await res.json();
-
-    return { 
-        props: {post: data}
-    }
-}
+};
 
 const BlogPost = ({ post }) => {
   return (

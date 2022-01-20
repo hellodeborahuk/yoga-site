ba0136dbe69f86f12ec4e6fdd0fd6ffb75a834f8
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-20 12:44:30 +0000

    Setup Firebase document store for blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,39 @@
 import styles from "../../styles/Blog.module.css";
-import Link from "next/link"
+import Link from "next/link";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPvh9ILI1N7j3-Mt_eH-uvkX13cdwoDaI",
+  authDomain: "yoga-website-4eafe.firebaseapp.com",
+  projectId: "yoga-website-4eafe",
+  storageBucket: "yoga-website-4eafe.appspot.com",
+  messagingSenderId: "343308541111",
+  appId: "1:343308541111:web:2d5d9292baab0b264b4132",
+};
+
+
 
 export async function getStaticProps() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
+  initializeApp(firebaseConfig);
+
+  const db = getFirestore();
+  const colRef = collection(db, "posts");
+  const data = await getDocs(colRef)
+    .then((snapshot) => {
+      let posts = [];
+      snapshot.docs.forEach((doc) => {
+        posts.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(posts);
+      return posts
+    })
+    .catch((err) => {
+      console.log;
+    });
+
+  //const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //const data = await res.json();
 
   return {
     props: { posts: data },
@@ -15,9 +45,12 @@ const Blog = ({ posts }) => {
     <div>
       <h1> Blog posts go here.</h1>
       {posts.map((post) => (
-        <Link href={`/blog/${post.id}`} key={post.id}>
-          <a className={styles.postTitle}>{post.title}</a>
-        </Link>
+        <div>
+          <Link href={`/blog/${post.id}`} key={post.id}>
+            <a className={styles.postTitle}>{post.title}</a>
+          </Link>
+          <p>{post.body}</p>
+        </div>
       ))}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Gallery from '../components/Gallery';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Gallery from "../components/Gallery";
+
 
 export default function Home() {
   return (

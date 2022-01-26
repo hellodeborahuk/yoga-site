03858142985750d9456273987877c0da20cf91a2
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-26 10:19:58 +0000

    Updated form with confirmation page and spam filter

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,57 @@
 import styles from "../styles/Contact.module.css";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 const Contact = () => {
+  const [submitterName, setSubmitterName] = useState("")
+  const router = useRouter();
+  const confirmationScreenVisible =
+    router.query?.success && router.query.success === "true";
+  const formVisible = !confirmationScreenVisible; 
+
+  const ConfirmationMessage = (
+    <React.Fragment>
+      <p>
+        Thank you for submitting this form. Someone should get back to you
+        within 24-48 hours.
+      </p>
+
+      <button
+        onClick={() => router.replace("/contact", undefined, { shallow: true })}
+      >
+        {" "}
+        Submit Another Response{" "}
+      </button>
+    </React.Fragment>
+  );
+
   const ContactForm = (
     <form
       method="POST"
       name="contact-form"
       action="contact/?success=true"
       data-netlify="true"
+      data-netlify-honeypot="bot-field"
       className={styles.form}
     >
       <input
         type="hidden"
-        name="form-name"
-        value="contact-form"
-        className={styles.input}
+        name="subject"
+        value={`You've got mail from ${submitterName}`}
       />
+      <input type="hidden" name="form-name" value="contact-form" />
+      <p hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" />
+        </label>
+      </p>
       <label htmlFor="name">Name *</label>
       <input
         id="name"
         name="name"
         required
         type="text"
+        onChange={(e) => setSubmitterName(e.target.value)}
         className={styles.input}
       />
       <label htmlFor="email">E-mail Address *</label>
@@ -48,10 +79,10 @@ const Contact = () => {
     <div className={styles.container}>
       <h1>Get in touch</h1>
       <p>
-        If you have any questions please complete the form below or reach out to me on
-        social media.
+        If you have any questions please complete the form below or reach out to
+        me on social media.
       </p>
-      {ContactForm}
+      {formVisible ? ContactForm : ConfirmationMessage}
       <p>
         Follow me on{" "}
         <a href="https://instagram.com/debbiedannyoga">Instagram</a> or{" "}

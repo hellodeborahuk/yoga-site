552bd6f81c24cd5fb2b3b66038323f2b534287d8
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-26 10:13:00 +0000

    added contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,60 @@
+import styles from "../styles/Contact.module.css";
+
 const Contact = () => {
+  const ContactForm = (
+    <form
+      method="POST"
+      name="contact-form"
+      action="contact/?success=true"
+      data-netlify="true"
+      className={styles.form}
+    >
+      <input
+        type="hidden"
+        name="form-name"
+        value="contact-form"
+        className={styles.input}
+      />
+      <label htmlFor="name">Name *</label>
+      <input
+        id="name"
+        name="name"
+        required
+        type="text"
+        className={styles.input}
+      />
+      <label htmlFor="email">E-mail Address *</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        required
+        className={styles.input}
+      />
+      <label htmlFor="message">Message *</label>
+      <textarea
+        id="message"
+        name="message"
+        required
+        className={styles.input}
+      ></textarea>
+      <button type="submit" className={styles.btn}>
+        Submit
+      </button>
+    </form>
+  );
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Get in touch</h1>
-      <p>If you have any questions please contact Debbie.</p>
-      <p>Email: debbiedannyoga@gmail.com </p>
       <p>
-        Follow me on <a href="https://instagram.com/debbiedannyoga">Instagram</a> or{" "}
+        If you have any questions please complete the form below or reach out to me on
+        social media.
+      </p>
+      {ContactForm}
+      <p>
+        Follow me on{" "}
+        <a href="https://instagram.com/debbiedannyoga">Instagram</a> or{" "}
         <a href="https://www.facebook.com/debbiedannyoga">Facebook</a>.
       </p>
     </div>
